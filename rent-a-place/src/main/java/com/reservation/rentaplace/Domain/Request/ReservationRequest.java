d3cdbc7c08e8039e3fec89ffac9084cc6748fca4
Kariyavula Gopika Manvitha <kxg210060@utdallas.edu>
2022-11-23 18:42:39 -0600

    DBMgr and Reservation request
    Pushing DBMgr and Reservation request changes into the repo

---
+++ rent-a-place/src/main/java/com/reservation/rentaplace/Domain/Request/ReservationRequest.java
@@ -0,0 +1,15 @@
+package com.reservation.rentaplace.Domain.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationRequest {
+    //Integer cartID;
+    String username;
+}
