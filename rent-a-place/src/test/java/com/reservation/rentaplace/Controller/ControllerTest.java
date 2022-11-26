8783c5962aa4d22bb4e966c7c64054b6ecdd8106
pritika priyadarshini <pritika98@gmail.com>
2022-11-26 15:14:04 -0600

    Added test cases for view cart

--- rent-a-place/src/test/java/com/reservation/rentaplace/Controller/ControllerTest.java
+++ rent-a-place/src/test/java/com/reservation/rentaplace/Controller/ControllerTest.java
@@ -1,9 +1,11 @@
 package com.reservation.rentaplace.Controller;
 
+import com.nimbusds.jose.shaded.json.JSONObject;
 import com.reservation.rentaplace.DAO.DBMgr;
 import com.reservation.rentaplace.Domain.*;
 import com.reservation.rentaplace.Domain.Request.CartRequest;
 import com.reservation.rentaplace.Domain.Request.CustomerRequest;
+import com.reservation.rentaplace.Domain.Request.HostPropertyRequest;
 import com.reservation.rentaplace.Exception.InvalidRequestException;
 import com.reservation.rentaplace.Exception.ResourceNotFoundException;
 import com.reservation.rentaplace.Exception.UnauthorizedException;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,8 +31,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.swing.text.html.parser.Entity;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -238,19 +245,27 @@ class ControllerTest {
         assertEquals("Unauthenticated - incorrect API Key.", exception.getMessage());
     }
 
-    @Test
-    @DisplayName("Checkout date before checkin date in add to cart")
-    void invalidCheckoutDatesAddToCart(){
+    Cart getEmptyCart(){
         Cart cart = new Cart();
         cart.setCartID(1);
         cart.setCartValue(0f);
         cart.setProperty(new ArrayList<RentalProperty>());
         cart.setCheckinDate(new ArrayList<Date>());
         cart.setCheckoutDate(new ArrayList<Date>());
+        return cart;
+    }
+    Customer getCustomer(){
         Customer customer = new Customer();
         customer.setUsername("cherry012");
         customer.setPassword("cher123");
         customer.setApiKey("xxxxx");
+        return customer;
+    }
+    @Test
+    @DisplayName("Checkout date before checkin date in add to cart")
+    void invalidDatesAddToCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
         customer.setCart(cart);
         CartRequest cr = new CartRequest();
         cr.setUsername("garry123");
@@ -262,22 +277,75 @@ class ControllerTest {
         when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
         when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn("villa");
         when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(property);
-        UnauthorizedException exception = assertThrows(UnauthorizedException.class, () -> c.addToCart(cr, "xxxxx"));
-        assertEquals("Unauthenticated - incorrect API Key.", exception.getMessage());
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> c.addToCart(cr, "xxxxx"));
+        assertEquals("Checkin date is after the checkout date, please select valid dates", exception.getMessage());
     }
 
-    @DisplayName("Invalid dates add to cart")
-    void invalidDatesAddToCart(){
-        Cart cart = new Cart();
-        cart.setCartID(1);
-        cart.setCartValue(0f);
-        cart.setProperty(new ArrayList<RentalProperty>());
-        cart.setCheckinDate(new ArrayList<Date>());
-        cart.setCheckoutDate(new ArrayList<Date>());
-        Customer customer = new Customer();
-        customer.setUsername("cherry012");
-        customer.setPassword("cher123");
-        customer.setApiKey("xxxxx");
+    @Test
+    @DisplayName("Invalid checkin date add to cart")
+    void invalidCheckinDatesAddToCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        CartRequest cr = new CartRequest();
+        cr.setUsername("garry123");
+        cr.setPropertyID(1);
+        cr.setCheckinDate("14-12-2022");
+        cr.setCheckoutDate("12-12-2022");
+        RentalProperty property = new Villa();
+        property.setPrice_per_night(70f);
+        when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
+        when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn("villa");
+        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(property);
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> c.addToCart(cr, "xxxxx"));
+        assertEquals("Invalid check-in date", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Past checkin date add to cart")
+    void pastCheckinDatesAddToCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        CartRequest cr = new CartRequest();
+        cr.setUsername("garry123");
+        cr.setPropertyID(1);
+        cr.setCheckinDate("11-18-2022");
+        cr.setCheckoutDate("12-12-2022");
+        RentalProperty property = new Villa();
+        property.setPrice_per_night(70f);
+        when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
+        when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn("villa");
+        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(property);
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> c.addToCart(cr, "xxxxx"));
+        assertEquals("Please select a future checkin date.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Past checkout date add to cart")
+    void pastCheckoutDatesAddToCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        CartRequest cr = new CartRequest();
+        cr.setUsername("garry123");
+        cr.setPropertyID(1);
+        cr.setCheckinDate("12-12-2022");
+        cr.setCheckoutDate("11-12-2022");
+        RentalProperty property = new Villa();
+        property.setPrice_per_night(70f);
+        when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
+        when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn("villa");
+        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(property);
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> c.addToCart(cr, "xxxxx"));
+        assertEquals("Please select a future checkout date.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Invalid checkout date add to cart")
+    void invalidCheckoutDatesAddToCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
         customer.setCart(cart);
         CartRequest cr = new CartRequest();
         cr.setUsername("garry123");
@@ -285,25 +353,34 @@ class ControllerTest {
         cr.setCheckinDate("12-12-2022");
         cr.setCheckoutDate("14-12-2022");
         RentalProperty property = new Villa();
-        property.setPrice_per_night(70f);
         when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
         when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn("villa");
-        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(property);
-        assertEquals("Added to cart successfully", c.addToCart(cr, "xxxxx"));
+        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(getProperty(property));
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> c.addToCart(cr, "xxxxx"));
+        assertEquals("Invalid check-out date", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Invalid property add to cart")
+    void invalidPropertyAddToCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        CartRequest cr = new CartRequest();
+        cr.setUsername("garry123");
+        cr.setPropertyID(1);
+        cr.setCheckinDate("12-12-2022");
+        cr.setCheckoutDate("12-15-2022");
+        when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
+        when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn(null);
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> c.addToCart(cr, "xxxxx"));
+        assertEquals("Invalid property id : "+ cr.getPropertyID(), exception.getMessage());
     }
     @Test
     @DisplayName("Successful add to cart")
     void successfulAddToCart(){
-        Cart cart = new Cart();
-        cart.setCartID(1);
-        cart.setCartValue(0f);
-        cart.setProperty(new ArrayList<RentalProperty>());
-        cart.setCheckinDate(new ArrayList<Date>());
-        cart.setCheckoutDate(new ArrayList<Date>());
-        Customer customer = new Customer();
-        customer.setUsername("cherry012");
-        customer.setPassword("cher123");
-        customer.setApiKey("xxxxx");
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
         customer.setCart(cart);
         CartRequest cr = new CartRequest();
         cr.setUsername("garry123");
@@ -311,12 +388,106 @@ class ControllerTest {
         cr.setCheckinDate("12-12-2022");
         cr.setCheckoutDate("12-14-2022");
         RentalProperty property = new Villa();
-        property.setPrice_per_night(70f);
         when(c.getDb().getCustomer(cr.getUsername())).thenReturn(customer);
         when(c.getDb().checkProperty(cr.getPropertyID())).thenReturn("villa");
-        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(property);
+        when(c.getDb().getProperty(cr.getPropertyID())).thenReturn(getProperty(property));
         when(c.getDb().updateCart(customer)).thenReturn(1);
         assertEquals("Added to cart successfully", c.addToCart(cr, "xxxxx"));
     }
 
+    @Test
+    @DisplayName("Invalid user view cart")
+    void invalidUserViewCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        when(c.getDb().getCustomer(customer.getUsername())).thenReturn(null);
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> c.viewCart(customer.getUsername(), "xxxxx"));
+        assertEquals("Invalid User", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Invalid session view cart")
+    void invalidSessionViewCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setApiKey(null);
+        customer.setCart(cart);
+        when(c.getDb().getCustomer(customer.getUsername())).thenReturn(customer);
+        UnauthorizedException exception = assertThrows(UnauthorizedException.class, () -> c.viewCart(customer.getUsername(), "yyyyy"));
+        assertEquals("Please login", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Invalid API key view cart")
+    void invalidAPIKeyViewCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        when(c.getDb().getCustomer(customer.getUsername())).thenReturn(customer);
+        UnauthorizedException exception = assertThrows(UnauthorizedException.class, () -> c.viewCart(customer.getUsername(), "yyyyy"));
+        assertEquals("Unauthenticated - incorrect API Key.", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Successful view empty cart")
+    void successfulViewEmptyCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        when(c.getDb().getCustomer(customer.getUsername())).thenReturn(customer);
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+        JSONObject price = new JSONObject();
+        price.put("Cart value", cart.getCartValue());
+        entities.add(price);
+
+        assertEquals(new ResponseEntity<Object>(entities, HttpStatus.OK), c.viewCart(customer.getUsername(),"xxxxx"));
+    }
+
+    @Test
+    @DisplayName("Successful view cart")
+    void successfulViewCart(){
+        Cart cart = getEmptyCart();
+        Customer customer = getCustomer();
+        customer.setCart(cart);
+        RentalProperty property = new Villa();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        try{
+            cart.addToCart(getProperty(property),sdf.parse("12-12-2022"), sdf.parse("12-15-2022"));
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        when(c.getDb().getCustomer(customer.getUsername())).thenReturn(customer);
+
+
+        assertEquals(new ResponseEntity<Object>(getViewResponse(customer.getCart(), sdf), HttpStatus.OK), c.viewCart(customer.getUsername(),"xxxxx"));
+    }
+
+    List<JSONObject> getViewResponse(Cart cart, SimpleDateFormat sdf){
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+        JSONObject entity = new JSONObject();
+        entity.put("Property", cart.getProperty().get(0));
+        String inDate = sdf.format(cart.getCheckinDate().get(0));
+        entity.put("Checkin date", inDate);
+        String outDate = sdf.format(cart.getCheckoutDate().get(0));
+        entity.put("Checkout date", outDate);
+        entities.add(entity);
+        JSONObject price = new JSONObject();
+        price.put("Cart value", cart.getCartValue());
+        entities.add(price);
+        return entities;
+    }
+    RentalProperty getProperty(RentalProperty property){
+        property.setPrice_per_night(70f);
+        property.setNum_bedrooms(3);
+        property.setAvailability(1);
+        property.setNum_baths(2);
+        property.setProperty_description("Lakeside villa");
+        property.setProperty_name("Marquis");
+        property.setProperty_type("Villa");
+        property.setCity("Madison");
+        property.setPet_friendly(1);
+        property.setWifi_avail(1);
+        property.setCarpet_area(1900);
+        return property;
+    }
 }
