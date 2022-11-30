35d1846fdb2bdefe10de6d09c9e6e75c03b76e4b
Kanya Krishi <kanyakrishi@Kanyas-MBP.lan>
2022-11-29 22:26:16 -0600

    Added test cases for host property, code and test cases for Rate Property

---
+++ rent-a-place/src/main/java/com/reservation/rentaplace/Domain/Request/RatePropertyRequest.java
@@ -0,0 +1,16 @@
+package com.reservation.rentaplace.Domain.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatePropertyRequest {
+    private  Integer reservationID;
+    private Integer propertyID;
+    private double rating;
+}
